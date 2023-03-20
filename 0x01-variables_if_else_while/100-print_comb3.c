d3241890a109d21bb55ee7c6e70ca05b38512267
cornelius-keino <corneliuskeino@gmail.com>
2023-03-20 04:03:31 -0700

    working with loops

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'reverse alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number1;
+int number2;
+
+for (number1 = 0; number1 < 9; number1++)
+{
+for (nunber2 = number1 + 1; number2 < 10; number2++)
+{
+putchar ((number1 % 10) + '0');
+putchar ((number2 % 10) + '0');
+
+if (number1 == 8 && number2 == 9)
+continue;
+
+putchar (',');
+putchar (' ');
+}
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'hexadecimal'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+char letter;
+
+for (number = 0; number < 10; number++)
+{
+putchar (number + '0');
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar (letter);
+}
+putchar('\n');
+return (0);
+}
