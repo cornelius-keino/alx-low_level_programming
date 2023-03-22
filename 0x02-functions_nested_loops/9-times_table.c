ad029cf72df78ca65590d051ddb463e682213e09
cornelius-keino <corneliuskeino@gmail.com>
2023-03-22 08:35:14 -0700

    all

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,30 @@
  */
 void times_table(void)
 {
-	int x, y, z, a, b;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-		z = x * y;
-		if (z > 9)
-		{
-			a = z % 10;
-			b = z / 10;
-			_putchar(44);
-			_putchar(32);
-			_putchar(a + '0');
-			_putchar(b + '0');
-		}
-		else
-		{
-			if (y != 0)
+			z = x * y;
+			if (y == 0)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
+				_putchar(z + '0');
 			}
-			_putchar(z + '0');
-		}
+			if (z <= 9 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
