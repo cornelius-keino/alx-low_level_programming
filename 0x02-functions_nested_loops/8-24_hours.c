92607f858ff6ca603bb2e2222b30b7f0bfc13199
cornelius-keino <corneliuskeino@gmail.com>
2023-03-22 07:56:13 -0700

    al

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,23 +11,20 @@ void jack_bauer(void)
 	{
 		for (h2 = 0; h2 <= 9; h2++)
 		{
-			/*if ((h1 <= 1 && h2 <= 9) || (h1 <= 2 && h2 <= 3))
-			{*/
 			for (m1 = 0; m1 <= 5; m1++)
 			{
-			for (m2 = 0; m2 <= 9; m2++)
-			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
 				if (h1 >= 2 && h2 >= 4)
 					break;
-			_putchar(h1 + '0');
-			_putchar(h2 + '0');
-			_putchar(58);
-			_putchar(m1 + '0');
-			_putchar(m2 + '0');
-			_putchar('\n');
+				_putchar(h1 + '0');
+				_putchar(h2 + '0');
+				_putchar(58);
+				_putchar(m1 + '0');
+				_putchar(m2 + '0');
+				_putchar('\n');
+				}
 			}
-			}
-			/*} */
 		}
 	}
 }
