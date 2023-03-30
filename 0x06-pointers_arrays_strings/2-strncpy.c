7d221ff1aec5da4ad28e2f049436e3448a533463
cornelius-keino <corneliuskeino@gmail.com>
2023-03-30 07:43:37 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - concatanates two srings with specific characters
+ * @dest: destination string
+ * @src: source string
+ * @n: number of strings from source
+ * Return: the concatanated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+
+return (dest);
+
+}
+
