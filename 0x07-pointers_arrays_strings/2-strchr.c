0e8b2a7846c6e78f0b6af83c37d644196ba5086c
cornelius-keino <corneliuskeino@gmail.com>
2023-04-04 02:22:12 -0700

    all

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strchar - sttring character
+ * _strchr - sttring character
  * @s: string pointer
  * @c: character to test
  * Return: character
@@ -9,15 +9,10 @@ char *_strchr(char *s, char c)
 {
 int i;
 
-for (i = 0; s[i] <= '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
-{
 return (s);
 }
-else
-{
 return (0);
-}
-}
 }
