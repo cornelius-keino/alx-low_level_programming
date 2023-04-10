ad9246931f44f5f59479b3f50acd85e0e2309061
cornelius-keino <corneliuskeino@gmail.com>
2023-04-10 06:44:49 -0700

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_program_name - print the name of a program
@@ -11,3 +12,18 @@ void print_program_name(char *name)
 	}
 	_putchar('\n');
 }
+/**
+ * main - entry point
+ * @argc: argument with number of command lines
+ * @argv: array of strings containing command line arguments
+ * Return: 0 on succsess
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		print_program_name(argv[0]);
+	}
+	return (0);
+}
+
