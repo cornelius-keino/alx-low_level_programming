79186b223f1af8a756a5d814013ddfb23b28233f
cornelius-keino <corneliuskeino@gmail.com>
2023-04-12 06:59:33 -0700

    malloc and free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+  * argstostr - cancatenate strings passed to arguments
+  * @ac: number of arguments
+  * @av: arguments passed
+  * Return: pointer to concatenated string
+  */
+char *argstostr(int ac, char **av)
+{
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+
+/* Compute the total length of the concatenated string */
+size_t total_length = 0;
+for (int i = 0; i < ac; i++)
+{
+total_length += strlen(av[i]) + 1; /* Add 1 for the newline character */
+}
+
+/* Allocate memory for the concatenated string */
+char *str = malloc((total_length + 1) * sizeof(char));
+if (str == NULL)
+{
+return (NULL);
+}
+
+/* Copy the arguments to the concatenated string,*/
+size_t offset = 0;
+for (int i = 0; i < ac; i++)
+{
+strcpy(str + offset, av[i]);
+offset += strlen(av[i]);
+str[offset++] = '\n';
+}
+str[offset] = '\0'; /* Add null terminator */
+
+return (str);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * strtow - Splits a string into words.
+ *
+ * @str: The string to split.
+ *
+ * Return: A pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || strcmp(str, "") == 0)
+	{
+		return (NULL);
+	}
+
+	int word_count = 0;
+	char **words = NULL;
+
+	/* Count the number of words in the string */
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			continue;
+		}
+
+		word_count++;
+
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			i++;
+		}
+	}
+
+	if (word_count == 0)
+	{
+		return (NULL);
+	}
+
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		return (NULL);
+	}
+
+	int word_index = 0;
+	int word_length = 0;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (word_length > 0)
+			{
+				words[word_index] = malloc((word_length + 1) * sizeof(char));
+				if (words[word_index] == NULL)
+				{
+					for (int j = 0; j < word_index; j++)
+					{
+						free(words[j]);
+					}
+					free(words);
+					return (NULL);
+				}
+				strncpy(words[word_index], str + i - word_length, word_length);
+				words[word_index][word_length] = '\0';
+				word_index++;
+				word_length = 0;
+			}
+			continue;
+		}
+
+		word_length++;
+	}
+
+	if (word_length > 0)
+	{
+		words[word_index] = malloc((word_length + 1) * sizeof(char));
+		if (words[word_index] == NULL)
+		{
+			for (int j = 0; j < word_index; j++)
+			{
+				free(words[j]);
+			}
+			free(words);
+			return (NULL);
+		}
+		strncpy(words[word_index], str + strlen(str) - word_length, word_length);
+		words[word_index][word_length] = '\0';
+		word_index++;
+	}
+
+	words[word_index] = NULL;
+
+	return (words);
+}
+
